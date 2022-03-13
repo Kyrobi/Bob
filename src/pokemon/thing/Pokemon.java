acab914c068fa93a46a1d3ef6b0e75446fe6e11a
TiredWeeb <pokemonxxlover@gmail.com>
2022-03-13 03:55:55 -0500

    stats
    idk

---
+++ src/pokemon/thing/Pokemon.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pokemon.thing;
+
+/**
+ *
+ * @author 15735
+ */
+public class Pokemon {
+    private int Level;
+    private float Exp = 0;
+    private int Health;
+    private int id;
+    private int HP, ATK, DEF, SPATK, SPDEF, SPD;
+    
+    
+    public Pokemon(int id)
+    {
+        this.id = id;
+        this.Level = 5;
+    }
+}
