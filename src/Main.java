ee62caec362e1615e897b2184a38555bd54caef6
Kyrobi <lightling123jr@gmail.com>
2022-03-13 05:33:12 -0500

    Re-organize

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import pokemon.Pokemon;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("test");
+
+        Pokemon pk = new Pokemon(3);
+
+        pk.test();
     }
 }
---
+++ src/pokemon/Pokemon.java
@@ -0,0 +1,36 @@
+package pokemon;
+
+public class Pokemon {
+
+    private int Level;
+    private int Exp = 0;
+    private int Health, MaxHealth;
+    private int Id;
+    private String Status;
+    private int HP, ATK, DEF, SPATK, SPDEF, SPD;
+
+    public Pokemon(int id)
+    {
+        this.Id = id;
+        this.Level = 5;
+    }
+
+    public void Damage(int Dmg)
+    {
+        int temp = this.Health - Dmg;
+
+        if (temp<=0)
+        {
+            this.Health = 0;
+            this.Status = "FNT";
+        }
+        else
+        {
+            this.Health = this.Health - Dmg;
+        }
+    }
+
+    public void test(){
+        System.out.println("Test works!");
+    }
+}
